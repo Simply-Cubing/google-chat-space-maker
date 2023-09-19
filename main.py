f3bb1ab940e7d7d939692c99c1fcceb6e15c7711
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2023-09-19 21:31:06 +0800

    added comments for what each click does

--- main.py
+++ main.py
@@ -9,22 +9,32 @@ def select_user():
 
 time.sleep(3)
 pyautogui.click(x = , y =)
+#clicks the "new space" button
 time.sleep(0.5)
 pyautogui.click(x = , y =)
+#clicks the "create space" button
 time.sleep(1)
 pyautogui.write(space_name)
+#writes the name of the space
 time.sleep(0.5)
 pyautogui.click(x = , y =)
+#clicks the space description field
 time.sleep(0.5)
 pyautogui.write(space_description)
+#writes the description
 time.sleep(0.5)
+#clicks the add user(s) field 
 pyautogui.click(x = , y =)
 time.sleep(0.5)
 
+#writes the user's name
 pyautogui.write("name")
+#selects it
 select_user()
-#repeat for all users
+#repeat for all users 
 
+#clicks off the user adding thing
+pyautogui.click(x =,y =)
+#clicks the "create" button
+pyautogui.click(x =,y =)
 
-pyautogui.click(x =,y =)
-pyautogui.click(x =,y =)
