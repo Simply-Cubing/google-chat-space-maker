874e742907dd5719f82210a942730ded6382af28
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2023-09-19 21:23:43 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+space_name = input("space name -> ")
+space_description = input("space description -> ")
+def select_user():
+    time.sleep(0.75)
+    pyautogui.click(x = , y =)
+    time.sleep(0.75)
+
+time.sleep(3)
+pyautogui.click(x = , y =)
+time.sleep(0.5)
+pyautogui.click(x = , y =)
+time.sleep(1)
+pyautogui.write(space_name)
+time.sleep(0.5)
+pyautogui.click(x = , y =)
+time.sleep(0.5)
+pyautogui.write(space_description)
+time.sleep(0.5)
+pyautogui.click(x = , y =)
+time.sleep(0.5)
+
+pyautogui.write("name")
+select_user()
+#repeat for all users
+
+
+pyautogui.click(x =,y =)
+pyautogui.click(x =,y =)
